279cb317205e3688a72dafff91ac4cbc9179648f
Katkauske <kkatkauske@gmail.com>
2022-10-11 11:49:00 +0300

    Ketvirta

---
+++ Ketvirta.js
@@ -0,0 +1,12 @@
+
+let x = 5;
+let y = 5;
+console.log(arSandaugaDidesneUzSuma(x,y))
+
+function arSandaugaDidesneUzSuma(x,y) {
+    if ((x * y) > (x + y)) 
+        return true
+    
+    else ((x * y) <= (x + y)) 
+        return false  
+}
